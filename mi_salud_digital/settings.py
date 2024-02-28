6c08911feecf9c5d45ed220235bdb8c588ff8ab7
David <dmg_92@hotmail.com>
2024-02-28 09:53:53 -0600

    authtoken

--- mi_salud_digital/settings.py
+++ mi_salud_digital/settings.py
@@ -39,7 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'specialists',
     'patients',
-    'consultations'
+    'consultations',
+    'rest_framework',
+    'rest_framework.authtoken'
 ]
 
 MIDDLEWARE = [
